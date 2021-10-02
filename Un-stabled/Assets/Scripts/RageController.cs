203e68bfccb3d8f8044b5b4f36a4f2c4cec9df4a
Keiran Young <kcyoung1997@hotmail.co.uk>
2021-10-02 17:52:34 +0100

    Add Icon to ragecontroller

--- Un-stabled/Assets/Scripts/RageController.cs
+++ Un-stabled/Assets/Scripts/RageController.cs
@@ -1,21 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RageController : MonoBehaviour
 {
+    private const float chargeRate = 0.04f;
+    private const float drainRate = 0.2f;
+
     private const int MinRage = 25;
     private float _rage;
     private float rage {
-        get { return _rage;}
+        get { return _rage; }
         set {
             _rage = value;
             bar.sizeDelta = new Vector2(200 * (_rage/max), 20);
+            icon.sprite = _rage > 75 || _isRaging ? sprite3 :
+                _rage > 50 ? sprite2 :
+                _rage > 25 ? sprite1 :
+                null;
         }
     }
+
     public float startRage;
     public float max;
+
+    public Image icon;
+    public Sprite sprite1;
+    public Sprite sprite2;
+    public Sprite sprite3;
+    
+
     public RectTransform bar;
+
     // Raging can only be started by this class
     private bool _isRaging;
     public void StartRage() {
@@ -35,10 +52,10 @@ public class RageController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.R)) StartRage();
 
         if (!_isRaging) {
-            rage += 0.1f;
+            rage += chargeRate;
             if (rage > max) rage = max;
         } else {
-            rage -= 0.5f;
+            rage -= drainRate;
             if (rage < 0) {
                 rage = 0;
                 _isRaging = false;
