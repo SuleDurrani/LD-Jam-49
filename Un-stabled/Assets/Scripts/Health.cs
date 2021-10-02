7af68d4ebd3dcadfb5466d153f30d42e2cab8fe7
Keiran Young <kcyoung1997@hotmail.co.uk>
2021-10-02 15:20:08 +0100

    Add start values and set max

--- Un-stabled/Assets/Scripts/Health.cs
+++ Un-stabled/Assets/Scripts/Health.cs
@@ -4,31 +4,53 @@ using UnityEngine;
 
 public class Health : MonoBehaviour
 {
-    public int current;
-    public int max;
+    private int _current;
+    private int current{
+        get { return _current; }
+        set {
+            if (healthBar != null) healthBar.UpdateHealth(value);
+            _current = value;
+        }
+    }
+    private int _max;
+    private int max{
+        get { return _max; }
+        set {
+            if (healthBar != null) healthBar.UpdateMax(value);
+            _max = value;
+        }
+    }
 
     public HealthBar healthBar;
+    public int startHealth;
+    public int startMax;
 
     // Start is called before the first frame update
     void Start()
     {
-        healthBar.UpdateHealth(current);
-        healthBar.UpdateMax(max);
+        current = startHealth;
+        max = startMax;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.RightArrow)) takeDamage(-1);
+        if(Input.GetKeyDown(KeyCode.RightArrow)) heal(1);
         if(Input.GetKeyDown(KeyCode.LeftArrow)) takeDamage(1);
     }
 
-    void takeDamage(int damage) {
-        current -= damage;
-        if (current < 0) {
-            current = 0;
-        }
-        healthBar.UpdateHealth(current);
+    public void takeDamage(int damage) {
+        if (current < damage) current = 0;
+        else current -= damage;
+    }
+
+    public void heal(int healing) {
+        if (current + healing > max) current = max;
+        else current += healing;
+    }
+
+    public void setMax(int newMax) {
+        max = newMax;
     }
 
 }
