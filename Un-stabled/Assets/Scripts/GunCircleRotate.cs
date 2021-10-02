f9339ab333cb23a79b034d28032a6568b5454b63
colin-mckay-campbell <91783210+colin-mckay-campbell@users.noreply.github.com>
2021-10-02 19:49:04 +0100

    Gun machine not broke

--- Un-stabled/Assets/Scripts/GunCircleRotate.cs
+++ Un-stabled/Assets/Scripts/GunCircleRotate.cs
@@ -6,9 +6,12 @@ public class GunCircleRotate : MonoBehaviour
 {
     Vector3 entityLocation; // This the "gun owner" position. e.g. The character holding the gun object
     Vector3 aimLocation;    // This is the aim location. 
-    
+    [SerializeField]
+    Rigidbody2D bulletObject;
     [SerializeField]
     private MouseFollower mouse;
+    // Bullet force
+    public int bulletStronk = 1000;
 
     // Start is called before the first frame update
     void Start()
@@ -23,8 +26,23 @@ public class GunCircleRotate : MonoBehaviour
         aimLocation = mouse.getMousePosition();
 
         float angle = angleFinder(entityLocation, aimLocation);
-        Debug.Log(angle.ToString());
+        //Debug.Log(angle.ToString());
         transform.rotation = Quaternion.Euler(0, 0, angle);
+
+
+        //shoot gun
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Rigidbody2D bullet = Instantiate(bulletObject);
+            //Black magic
+            Vector3 direction = (Vector2)(Quaternion.Euler(0,0,angle) * Vector2.right);
+            //EOBlack magic
+            bullet.AddForce(direction * bulletStronk);
+            bullet.transform.position = transform.position;
+            bullet.transform.rotation = Quaternion.Euler(0, 0, angle);
+            Debug.Log("BANG");
+        }
     }
 
     private float angleFinder(Vector3 entity, Vector3 aim) 
@@ -35,4 +53,5 @@ public class GunCircleRotate : MonoBehaviour
         float degrees = radians * Mathf.Rad2Deg;
         return degrees;
     }
+
 }
